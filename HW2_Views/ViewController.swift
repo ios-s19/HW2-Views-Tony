6a9f937522d1247a05f5305d789fa68832551c5d
Tony Aiello <tonyaiello@AppFactory-Sysadmins-Mac-Pro.local>
2019-03-01 11:30:03 -0600

    First commit today - setting it all up

---
+++ HW2_Views/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  HW2_Views
+//
+//  Created by Tony Aiello on 2/28/19.
+//  Copyright © 2019 Tony Aiello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let container = CGRect(x: 0, y: 0, width: 200, height: 200)
+//        let main = UIView(frame: container)
+//        main.backgroundColor = UIColor.blue
+//        view.addSubview(main)
+//
+//        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.green
+////        view.addSubview(secondView)
+//        main.addSubview(secondView)
+        
+        
+        
+    }
+
+
+}
+
